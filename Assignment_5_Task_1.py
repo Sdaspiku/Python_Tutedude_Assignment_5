5e9b7cb06120c5ec98e3dd546595a96423f70223
Sdaspiku <das.souvikd@gmail.com>
2025-05-01 15:25:37 +0530

    Add files via upload

---
+++ Assignment_5_Task_1.py
@@ -0,0 +1,10 @@
+try:
+    
+    marksheet = {'Aakash':97, "Binoo":95, "Cyrus":96,"Daniel":91,"Ellina":94}
+    n = input("Enter the name of the student: ")
+    print("Name: ",n, ", Marks: ",marksheet[n])
+
+except:
+    print("Name not found. Please check.")
+    
+    
---
+++ Assignment_5_Task_2.py
@@ -0,0 +1,9 @@
+List_1=[]
+for i in range(1,10,1):
+    List_1.append(i)
+print(List_1)
+List_5=(List_1[0:5])
+print(List_5)
+List_rev=List_5.reverse()
+
+print(List_5[-5:])
